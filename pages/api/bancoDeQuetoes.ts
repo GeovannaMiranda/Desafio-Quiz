f782908662213a0b717052d6b0f7db6a68f685f2
Geovanna Miranda <72633976+GeovannaMiranda@users.noreply.github.com>
2022-12-01 14:50:56 -0300

    Add files via upload

--- pages/api/bancoDeQuetoes.ts
+++ pages/api/bancoDeQuetoes.ts
@@ -26,6 +26,43 @@ const questoes: QuestaoModel[] = [
         RespostaModel.errada('Trapézio'),
         RespostaModel.certa('Escaleno'),
     ]),
+    new QuestaoModel(205, 'Quem compôs o hino da independência?', [
+        RespostaModel.errada('Manuel Bandeira'),
+        RespostaModel.errada('Castro Alves'),
+        RespostaModel.errada('Carlos Gomes'),
+        RespostaModel.certa('Dom Pedro I'),
+
+    ]),
+    new QuestaoModel(206, 'Qual é o antônimo de "malograr"?', [
+        RespostaModel.errada('Perder'),
+        RespostaModel.errada('Fracassar'),
+        RespostaModel.errada('Desprezar'),
+        RespostaModel.certa('Conseguir'),
+    ]),
+    new QuestaoModel(207, 'Em que país nasceu Carmem Miranda?', [
+        RespostaModel.errada('Brasil'),
+        RespostaModel.errada('Espanha'),
+        RespostaModel.errada('Argentina'),
+        RespostaModel.certa('Portugal'),
+    ]),
+    new QuestaoModel(208, 'Qual foi o último presidente no período da ditadura militar no Brasil?', [
+        RespostaModel.errada('Emílio Maciel'),
+        RespostaModel.errada('Ernesto Geisel'),
+        RespostaModel.errada('Costa e Silva'),
+        RespostaModel.certa('João Figuereido'),
+    ]),
+    new QuestaoModel(209, 'Seguindo a sequência do baralho qual carta vem depois do dez?', [
+        RespostaModel.errada('Rei'),
+        RespostaModel.errada('Nove'),
+        RespostaModel.errada('Às'),
+        RespostaModel.certa('Valete'),
+    ]),
+    new QuestaoModel(210, 'O adjetivo "venoso" está relacionado a?', [
+        RespostaModel.errada('Vela'),
+        RespostaModel.errada('Vento'),
+        RespostaModel.errada('Vênia'),
+        RespostaModel.certa('Veia'),
+    ]),
 
 ]
 export default questoes;
